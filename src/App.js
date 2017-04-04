47f1d7ea16669483338280f6bcae4e0f6d657107
coreydunkin <corey.dunkin@gmail.com>
2017-04-04 23:18:05 +1000

    start

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+var USER_DATA = {
+  name: 'Corey Dunkin',
+  username: 'corey_irl',
+  image: 'https://avatars0.githubusercontent.com/u/2933430?v=3$s=460'
+}
+
+var ReactDOM = require('react-dom');
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p> 
+      </div>
+    );
+  }
+
+}
+
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}}></img>
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <a href={'https://www.github.com/' +  this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function (){
+    return <div>{this.props.name}</div>
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return(
+      <div>
+        <ProfilePic imageUrl={this.props.user.image}/>
+        <ProfileName name={this.props.user.name}/>
+        <ProfileLink username={this.props.user.username}/>
+      </div>
+    );
+  }
+});
+
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('fb'));
+
+class ShowList extends App {
+
+}
+
+
+export default App;
